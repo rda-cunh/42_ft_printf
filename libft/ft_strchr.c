d6402485ca257bc4bcbf86e66af37c86aefa6f30
Ricardo Mendes <147749667+rda-cunh@users.noreply.github.com>
2023-11-21 22:18:31 +0000

    Add files via upload

---
+++ libft/ft_strchr.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rda-cunh <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/24 18:39:36 by rda-cunh          #+#    #+#             */
+/*   Updated: 2023/11/08 00:27:39 by rda-cunh         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strchr(const char *s, int c)
+{
+	char	altc;
+	char	*alts;
+
+	altc = c;
+	alts = (char *)s;
+	while (*alts != '\0')
+	{
+		if (*alts == altc)
+			return (alts);
+		alts++;
+	}
+	if (altc == 0)
+		return (alts);
+	return (NULL);
+}
+/*
+#include <stdio.h>
+#include <string.h>
+
+int	main(void)
+{
+	char str[] = "Vai ser um teste";
+	char c = 't';
+
+	printf("My function: %s\n", ft_strchr(str, c));
+	printf("My function: %s\n", strchr(str, c));
+}
+*/
---
+++ libft/ft_strjoin.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rda-cunh <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/02 11:49:47 by rmendes           #+#    #+#             */
+/*   Updated: 2023/11/08 01:02:41 by rda-cunh         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*str;
+	size_t	s1_len;
+	size_t	s2_len;
+
+	if (!s1 || !s2)
+		return (NULL);
+	s1_len = ft_strlen(s1);
+	s2_len = ft_strlen(s2);
+	str = (char *)malloc(sizeof(char) * (s1_len + s2_len + 1));
+	if (!str)
+		return (NULL);
+	ft_memcpy(str, s1, s1_len);
+	ft_memcpy(str + s1_len, s2, s2_len);
+	str[s1_len + s2_len] = '\0';
+	return (str);
+}
+/*
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+	char s1[] = "My name is Mario";
+	char s2[] = ", Super Mario"; 
+
+	printf("%s\n", ft_strjoin(s1, s2));	
+	return (0);
+}*/
