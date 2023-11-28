a076798a0b98ff9f3414018d6f18abf51060c8a8
rda-cunh <ricard.mend@gmail.com>
2023-11-28 19:11:02 +0000

    new

--- ft_printf.h
+++ ft_printf.h
@@ -6,12 +6,12 @@
 /*   By: rda-cunh <rda-cunh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/28 18:02:44 by rda-cunh          #+#    #+#             */
-/*   Updated: 2023/11/28 18:23:19 by rda-cunh         ###   ########.fr       */
+/*   Updated: 2023/11/28 19:10:47 by rda-cunh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_PRINTF_H
-# define FT_PRINTF_H
+#ifndef LIBFTPRINTF_H
+# define LIBFTPRINTF_H
 
 #include <stdarg.h>
 #include <stdio.h>
